96cf20a1b87475f9fa9ea8cae5cec86a92ec611c
shabnomnom <shabnam.shomail@gmail.com>
2022-07-19 03:05:33 +0200

    delete icon and functionality added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,35 @@
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+import {useState} from 'react' //hook
 
 const App = () => {
+  const [tasks, setTasks] =  useState( 
+    [
+      {
+        "id": 1,
+        "text": "Doctors Appointment",
+        "day": "Feb 5th at 2:30pm",
+        "reminder": true
+      },
+      {
+        "id": 2,
+        "text": "Meeting at School",
+        "day": "Feb 6th at 1:30pm",
+        "reminder": true
+      }
+    ]
+  )
+
+  // delete task 
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
   return (
     <div className='container'>
       <Header/>
-    </div>
-         
+      {tasks.length > 0 ?<Tasks tasks={tasks} onDelete={deleteTask}/>
+      : 'There is no more tasks' }
+    </div>       
   );
 }
 
